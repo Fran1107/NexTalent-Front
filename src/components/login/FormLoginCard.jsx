1db006ad9d40a0d70e35c86d31d5739257e7016b
Karen Cordoba <karen_cochi_02@hotmail.com>
2025-10-26 20:29:33 -0300

    boton de google con fondo gradiente

--- src/components/login/FormLoginCard.jsx
+++ src/components/login/FormLoginCard.jsx
@@ -10,7 +10,7 @@ export default function FormLoginCard() {
           <div className="bg-white rounded-3xl shadow-lg p-8 border border-gray-200">
 
             {/* /* Botón de Google */}
-            <button className="w-full bg-gradient-to-r from-blue-400 via-purple-300 to-pink-300 text-gray-800 font-semibold py-3 px-6 rounded-full flex items-center justify-center gap-3 mb-6 hover:opacity-90 transition ">
+            <button className="w-full bg-[url('/img/bgc-google.png')] bg-cover bg-center text-gray-800 font-semibold py-3 px-6 rounded-full flex items-center justify-center gap-3 mb-6 hover:opacity-90 transition border border-blue">
               <svg className="w-6 h-6" viewBox="0 0 24 24">
                 <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
                 <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
