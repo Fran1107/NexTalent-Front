0cdaa222248a56ed0c5c5d57c1116330ffb1ed0d
Karen Cordoba <karen_cochi_02@hotmail.com>
2025-11-01 20:09:15 -0300

    login de empresas creado

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/Layout"
 import IndexView from "./views/Home/IndexView"
 import AboutUsView from "./views/aboutus/AboutUsView"
 import LoginView from "./auth/LoginView"
+import LoginEmpresa from "./auth/LoginEmpresa"
 
 export default function Router() {
     return (
@@ -12,6 +13,7 @@ export default function Router() {
                     <Route index element={<IndexView />} />
                     <Route path="about" element={<AboutUsView />} />
                     <Route path="auth/login" element={<LoginView />} />
+                    <Route path="auth/login-empresas" element={<LoginEmpresa />} />
                 </Route>
             </Routes>
         </BrowserRouter>
