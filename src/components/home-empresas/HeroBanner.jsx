a9226d1200273866f8db28d8f8ca4e26d39c3ec5
Karen Cordoba <karen_cochi_02@hotmail.com>
2025-11-09 16:28:30 -0300

    Página de inicio para empresas creado

--- src/components/home-empresas/HeroBanner.jsx
+++ src/components/home-empresas/HeroBanner.jsx
@@ -1,23 +1,31 @@
+import { NavLink } from "react-router-dom";
+
 export default function HeroBanner({ image }) {
   return (
     <section
       className="w-full h-[60vh] bg-cover bg-center  shadow-lg overflow-hidden"
       style={{ backgroundImage: `url(${image})` }}
     >
-
-      <div className="w-full h-full bg-black/50 flex items-center justify-end">
-      <div className="text-white text-end mr-8">
-        <h2 className="text-3xl font-bold mb-2">Publicá tus búsquedas de</h2>
-        <h2 className="text-3xl font-bold mb-2">talento en un sólo lugar</h2>
-        {/* boton de ingresar */}
-        <button type="button" className="w-[20vh] bg-[#4D1874] text-white font-semibold py-3 px-6 rounded-full hover:bg-[#3b115a] transition mr-30 border border-[#BA96D5]">Ingresar</button>        
+      {/* Mensaje en el hero banner con botón para ingresar a la cuenta */}
+      <div className="w-full h-full bg-black/50 flex justify-end items-center text-right px-6 sm:px-12">
+        <div className="text-white max-w-lg">
+          <h2 className="text-2xl sm:text-3xl font-bold mb-2">
+            Publicá tus búsquedas de
+          </h2>
+          <h2 className="text-2xl sm:text-3xl font-bold mb-6">
+            talento en un sólo lugar
+          </h2>
+          {/* Botón de ingresar */}
+            <NavLink to="/auth/login-empresas">
+              <button
+                type="button"
+                className="w-[200px] bg-[#4D1874] text-white font-semibold py-3 rounded-full hover:bg-[#3b115a] transition border border-[#BA96D5]"
+              >
+                Ingresar              
+              </button>
+          </NavLink>
+        </div>
       </div>
-      </div>
-      {/* Sección de texto con números de pasantes, etc */}
-      {/* <div className="bg-[#4D1874] w-full h-[60vh]">
-        <h3>+20.000</h3>
-        <h3>Empresas registradas</h3>
-      </div>       */}
     </section>
 
   )
--- src/views/home/IndexEmpresasView.jsx
+++ src/views/home/IndexEmpresasView.jsx
@@ -1,11 +1,22 @@
+import Estadisticas from "../../components/home-empresas/Estadisticas";
 import HeroBanner from "../../components/home-empresas/HeroBanner";
 
 export default function IndexEmpresasView() {
 
     return (
-        <div className="w-full h-full ">
-            <HeroBanner image="/img/people-background.jpg" />
-        </div>
+        <>
+            <div className="w-full">
+                <HeroBanner image="/img/people-background.jpg" />
+            </div>
+            <div className="bg-[#4D1874] w-full py-6">
+                <div className="flex flex-col sm:flex-row sm:flex-wrap justify-around items-center gap-6 text-center">
+                    <Estadisticas h2="+20.000" p="Empresas registradas" />
+                    <Estadisticas h2="+50.000" p="CV cargados" />
+                    <Estadisticas h2="+10.000" p="Postulaciones mensuales" />
+                    <Estadisticas h2="+10.000" p="Avisos publicados" />
+                </div>
+            </div>
 
+        </>
     )
 }
