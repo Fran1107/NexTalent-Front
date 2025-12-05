138f5336c5d849b9b1344522b5f8a3365852f960
Alejandro663 <omarluna7787@gmail.com>
2025-12-05 16:42:33 -0300

    Solucion de merge conflictivo con login

--- src/API/pasanteApi.js
+++ src/API/pasanteApi.js
@@ -65,3 +65,53 @@ export async function deleteMyPasanteAccount() {
         throw error;
     }
 }
+
+
+
+// --- GET: Obtener mi perfil ---
+export async function getMyProfile() {
+    try {
+        const { data } = await api.get('/pasantes/profile/me');
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) throw error.response.data;
+        throw new Error("Error desconocido");
+    }
+}
+
+// --- GET: Obtener perfil público (por ID) ---
+export async function getPublicProfile(id) {
+    try {
+        const { data } = await api.get(`/pasantes/${id}`);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) throw error.response.data;
+        throw new Error("Error desconocido");
+    }
+}
+
+// --- POST: Subir Foto de Perfil ---
+export async function uploadFotoPerfil(formData) {
+    try {
+        const { data } = await api.post('/pasantes/upload-foto', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' } // Importante para archivos
+        });
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) throw error.response.data;
+        throw new Error("Error al subir imagen");
+    }
+}
+
+// --- POST: Subir CV ---
+export async function uploadCV(formData) {
+    try {
+        const { data } = await api.post('/pasantes/upload-cv', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        });
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) throw error.response.data;
+        throw new Error("Error al subir CV");
+    }
+}
